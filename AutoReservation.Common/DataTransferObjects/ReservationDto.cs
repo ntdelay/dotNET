a38e8cf23936ef1c26b0e95fdc0b9917a909709a
adelay@hsr.ch <adelay@hsr.ch>
2016-12-17 15:30:07 +0100

    Common aktualisiert

--- AutoReservation.Common/DataTransferObjects/ReservationDto.cs
+++ AutoReservation.Common/DataTransferObjects/ReservationDto.cs
@@ -1,57 +1,78 @@
 ﻿using AutoReservation.Common.DataTransferObjects.Core;
+using System;
+using System.Runtime.Serialization;
+using System.Text;
 
 namespace AutoReservation.Common.DataTransferObjects
 {
-    public class ReservationDto //: DtoBase<ReservationDto>
+    [DataContract]
+    public class ReservationDto : DtoBase<ReservationDto>
     {
+        [DataMember]
+        public int ReservationsNr { get; set; }
 
-        //public override string Validate()
-        //{
-        //    StringBuilder error = new StringBuilder();
-        //    if (Von == DateTime.MinValue)
-        //    {
-        //        error.AppendLine("- Von-Datum ist nicht gesetzt.");
-        //    }
-        //    if (Bis == DateTime.MinValue)
-        //    {
-        //        error.AppendLine("- Bis-Datum ist nicht gesetzt.");
-        //    }
-        //    if (Von > Bis)
-        //    {
-        //        error.AppendLine("- Von-Datum ist grösser als Bis-Datum.");
-        //    }
-        //    if (Auto == null)
-        //    {
-        //        error.AppendLine("- Auto ist nicht zugewiesen.");
-        //    }
-        //    else
-        //    {
-        //        string autoError = Auto.Validate();
-        //        if (!string.IsNullOrEmpty(autoError))
-        //        {
-        //            error.AppendLine(autoError);
-        //        }
-        //    }
-        //    if (Kunde == null)
-        //    {
-        //        error.AppendLine("- Kunde ist nicht zugewiesen.");
-        //    }
-        //    else
-        //    {
-        //        string kundeError = Kunde.Validate();
-        //        if (!string.IsNullOrEmpty(kundeError))
-        //        {
-        //            error.AppendLine(kundeError);
-        //        }
-        //    }
+        [DataMember]
+        public DateTime Von { get; set; }
 
-        //    if (error.Length == 0) { return null; }
+        [DataMember]
+        public DateTime Bis { get; set; }
 
-        //    return error.ToString();
-        //}
+        [DataMember]
+        public AutoDto Auto { get; set; }
 
-        //public override string ToString()
-        //    => $"{ReservationsNr}; {Von}; {Bis}; {Auto}; {Kunde}";
+        [DataMember]
+        public KundeDto Kunde { get; set; }
+
+        [DataMember]
+        public byte[] RowVersion { get; set; }
+
+        public override string Validate()
+        {
+            StringBuilder error = new StringBuilder();
+            if (Von == DateTime.MinValue)
+            {
+                error.AppendLine("- Von-Datum ist nicht gesetzt.");
+            }
+            if (Bis == DateTime.MinValue)
+            {
+                error.AppendLine("- Bis-Datum ist nicht gesetzt.");
+            }
+            if (Von > Bis)
+            {
+                error.AppendLine("- Von-Datum ist grösser als Bis-Datum.");
+            }
+            if (Auto == null)
+            {
+                error.AppendLine("- Auto ist nicht zugewiesen.");
+            }
+            else
+            {
+                string autoError = Auto.Validate();
+                if (!string.IsNullOrEmpty(autoError))
+                {
+                    error.AppendLine(autoError);
+                }
+            }
+            if (Kunde == null)
+            {
+                error.AppendLine("- Kunde ist nicht zugewiesen.");
+            }
+            else
+            {
+                string kundeError = Kunde.Validate();
+                if (!string.IsNullOrEmpty(kundeError))
+                {
+                    error.AppendLine(kundeError);
+                }
+            }
+
+            if (error.Length == 0) { return null; }
+
+            return error.ToString();
+        }
+
+        public override string ToString()
+            => $"{ReservationsNr}; {Von}; {Bis}; {Auto}; {Kunde}";
 
     }
 }
