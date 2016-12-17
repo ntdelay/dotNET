7d94c4118ced921c881d8b01b6e5019b73c38122
Adrian Froehlich <a1froehl@hsr.ch>
2016-12-17 15:31:41 +0100

    no message

--- AutoReservation.Dal/Entities/Kunde.cs
+++ AutoReservation.Dal/Entities/Kunde.cs
@@ -5,5 +5,23 @@ using System.Data.Entity;
 
 namespace AutoReservation.Dal.Entities
 {
-  
+  public class Kunde {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Lastname { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
+
+
+
+
+
+    }
+
 }
--- AutoReservation.Dal/Entities/Reservation.cs
+++ AutoReservation.Dal/Entities/Reservation.cs
@@ -6,6 +6,20 @@ namespace AutoReservation.Dal.Entities
 {
     public class Reservation
     {
-
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ReservationNr { get; set; }
+        [ForeignKey("Auto")]
+        public int CarId { get; set; }
+        [ForeignKey("Kunde")]
+        public int ClientId { get; set; }
+        public virtual Auto Car { get; set; }
+        public virtual Kunde Client { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime From { get; set; } 
+        [Column(TypeName = "datetime")]
+        public DateTime To { get; set; }
+        [Timestamp]
+        public byte[] RowVersion { get; set; }  
     }
 }
